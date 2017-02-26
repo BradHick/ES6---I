2efbec9705c776053c7f79127487fdeeeb1940b9
Brad Hick <bradleyhrm@gmail.com>
2017-02-26 15:30:22 -0300

    Adiionando mensagens na aplicação
    trablahndo com mengsans no caso de adição de negociações na aplicação.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,6 +7,9 @@ class NegociacaoController {
     this._listaNegociacoes = new ListaNegociacoes();
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
     this._negociacoesView._update(this._listaNegociacoes);
+    this._mensagem = new Mensagem();
+    this._mensagemView = new MensagemView($('#mensagemView'));
+    this._mensagemView._update(this._mensagem);
   }
 
   _criaNegociacao(){
@@ -22,6 +25,9 @@ class NegociacaoController {
 
     this._listaNegociacoes.adiciona(this._criaNegociacao());
     this._negociacoesView._update(this._listaNegociacoes);
+
+    this._mensagem.texto = "Negociação adicionada com sucesso!";
+    this._mensagemView._update(this._mensagem);
     console.log(this._listaNegociacoes.negociacoes);
     this._limpaFormulario();
 
