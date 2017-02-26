424ff91d8ef020f3986aaa38ffdd79068d69c90a
Brad Hick <bradleyhrm@gmail.com>
2017-02-26 15:43:07 -0300

    Usando a sobreescrita com js

--- aluraframe/client/js/app/views/View.js
+++ aluraframe/client/js/app/views/View.js
@@ -4,6 +4,10 @@ class View {
   }
 
 
+  _template(){
+    throw new Error("O método template deve ser implementado.");
+  }
+
   _update(model){
     this._elemento.innerHTML = this._template(model);
   }
